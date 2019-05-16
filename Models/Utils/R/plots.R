cd6e407be1cf1c7cc1fc417e98023cabd2b171e4
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-05-16 09:26:32 +0200

    .

--- Models/Utils/R/plots.R
+++ Models/Utils/R/plots.R
@@ -16,6 +16,11 @@ parstr<-function(val){
 }
 
 
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
 
 # function to get single param points from raw result
 #  --> make generic function ?
